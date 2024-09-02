952c3ac2895737c21b2da4ad50bf9616864ff9ec
Jeremy Scala <scalajeremy@gmail.com>
2024-09-02 16:55:40 +0200

    wip

---
+++ data.js
@@ -0,0 +1,54 @@
+const data = {
+  value: 0,
+  children: [
+    {
+      name: "Le Monde des Possibles",
+      value: 4,
+      nodeSettings: {
+        fill: am5.color(0x297373)
+      },
+      children: [
+        {
+          name: "Service Juridique",
+          value: 3,
+          children: [
+            {
+              name: "Liège",
+              value: 2,
+              children: [
+                {name: "CNCD 11.11.11", value:1},
+                {name: "Collectif de Résistance Aux Centres Pour Etrangers (CRACPE)", value:1},
+                {name: "Plateforme d'hébergement citoyenne de Liège", value:1},
+                {name: "Promotion & Culture", value:1},
+                {name: "Service Droit des Jeunes", value:1},
+                {name: "Tabane", value:1},
+                {name: "Sida Sol", value:1},
+                {name: "Centre Régional pour l'Intégration des Personnes Étrangères (CRIPEL)", value:1},
+                {name: "Centre for Ethnic and Migration Studies (CEDEM)", value:1},
+                {name: "Planning familial Louise Michel", value:1},
+                {name: "Collectif contre les Violences Familiales et l'Exclusion (CVFE)", value:1},
+                {name: "Point d'Appui", value:1},
+                {name: "FGTB Liège-Huy-Waremme", value:1},
+                {name: "Théâtre de Liège", value:1},
+                {name: "La Casa Nicaragua", value:1},
+                {name: "Collectif Liège Hospitalière", value:1},
+                {name: "Solidaris", value:1},
+                {name: "Voix des sans-papiers de Liège", value:1},
+              ]
+            },
+            {
+              name: "Bruxelles",
+              value: 2,
+            },
+            {
+              name: "Wallonie",
+              value: 2,
+            }
+          ]
+        },
+      ]
+    },
+  ]
+};
+
+export { data };
